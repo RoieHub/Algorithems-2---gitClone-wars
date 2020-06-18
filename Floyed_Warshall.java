c603dcdd48e1105e53503ea7810ac688f2b26f8b
RoieJinx <roiemalykin@gmail.com>
2020-06-18 15:48:29 +0300

    Test commit

--- Floyed_Warshall.java
+++ Floyed_Warshall.java
@@ -4,15 +4,65 @@ public class Floyed_Warshall
 
 	public static void main(String[] args)
 	{
-		// Without path
-		int m = 1;
-		int n = 2;
-		boolean[][] mat = BottleProblem.BottlesProblem1(m, n);
+		/*
+		 * Without path int m = 1; int n = 2; boolean[][] mat =
+		 * BottleProblem.BottlesProblem1(m, n); floyed_warshall(mat);
+		 * 
+		 * MyFunctions.printMatt(mat); Why does this generic func Dont work with
+		 * boolean? need to fix For now use specefied function
+		 * MyFunctions.printMatt(mat);
+		 */
 		
-		//MyFunctions.printMatt(mat); Why does this generic func Dont work with boolean? need to fix
-		//For now use specefied function
-		MyFunctions.printMatt(mat);
+		int m = 3;
+		int n = 5;
+		boolean[][] mat = BottleProblem.BottlesProblem1(m, n);
+		int size = mat.length;
+		String[][] path = InitPath(size , mat , n);
+		floyd_warshall(mat,path);
+		FixPath(path , n);
+		System.out.println( "Bottle 1 = "+ m +" , Bottle 2 = "+n);
+		
+		IsExist(0,0,0,4,path,n);
+		
+		System.out.println("\nAll paths : \n");
+		
+		
 
 	}
-//TEST
+	
+	public static void floyed_warshall(boolean[][] mat)
+	{
+		int size = mat.length;
+		for(int k = 0 ; k < size ; k++)
+		{
+			for(int i = 0 ; i < size ; i++)
+			{
+				for( int j = 0 ; j < size ; j++)
+				{
+					mat[i][j] = (mat[i][k] && mat[k][j] || mat[i][j]);
+				}
+			}
+		}
+	}
+	
+	public static void floyd_warshall(boolean[][] mat , String[][] paths)
+	{
+		int size = mat.length;
+		for( int k = 0 ; k < size ; k++)
+		{
+			for( int i = 0 ; i < size ; i++)
+			{
+				for( int j = 0 ; j < size ; j++)
+				{
+					if(mat[i][j] == false && ((mat[i][k] && mat[k][j]) == true))
+					{
+						paths[i][j] = paths[i][k] + "->"+ paths[k][j];
+					}
+					mat[i][j] = (mat[i][k] && mat[k][j])||mat[i][j];
+				}
+			}
+		}
+	}
+	
+
 }
--- MyFunctions.java
+++ MyFunctions.java
@@ -6,9 +6,9 @@ public class MyFunctions
 	{
 		int row_size = mat.length;
 		int col_size = mat[0].length;
-		for( int r = 0 ; r <= row_size ; r++)
+		for( int r = 0 ; r < row_size ; r++)
 		{
-			for( int c = 0 ; c <= col_size ; c++)
+			for( int c = 0 ; c < col_size ; c++)
 			{
 				System.out.print(mat[r][c]);
 				System.out.print("  ,  ");
@@ -23,15 +23,15 @@ public class MyFunctions
 	{
 		int row_size = mat.length;
 		int col_size = mat[0].length;
-		for( int r = 0 ; r <= row_size ; r++)
+		for( int r = 0 ; r < row_size ; r++)
 		{
-			for( int c = 0 ; c <= col_size ; c++)
+			for( int c = 0 ; c < col_size ; c++)
 			{
 				System.out.print(mat[r][c]);
 				System.out.print("  ,  ");
 				
 			}
-			System.out.println(); // line down
+			System.out.println(""); // line down
 		}
 		
 	}
