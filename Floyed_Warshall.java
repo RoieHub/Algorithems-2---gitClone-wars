d38902b2d4ad19eddf2173a0442fccd7034e9ed4
CyberLab <CyberLab@DESKTOP-3AKGVA0>
2020-06-18 13:31:14 +0300

    generric func printMatt not working, should be fixed, using now a specefied boolean ver

--- Floyed_Warshall.java
+++ Floyed_Warshall.java
@@ -7,9 +7,11 @@ public class Floyed_Warshall
 		// Without path
 		int m = 1;
 		int n = 2;
-		boolean[][] T = BottleProblem.BottlesProblem1(m, n);
+		boolean[][] mat = BottleProblem.BottlesProblem1(m, n);
 		
-		
+		//MyFunctions.printMatt(mat); Why does this generic func Dont work with boolean? need to fix
+		//For now use specefied function
+		MyFunctions.printMatt(mat);
 
 	}
 
--- MyFunctions.java
+++ MyFunctions.java
@@ -1,5 +1,5 @@
 
-public class MyFuctions
+public class MyFunctions
 {
 	
 	public static < T > void printMatt( T [][] mat)
@@ -10,7 +10,25 @@ public class MyFuctions
 		{
 			for( int c = 0 ; c <= col_size ; c++)
 			{
-				System.out.println(mat[r][c] + "  ,  " );
+				System.out.print(mat[r][c]);
+				System.out.print("  ,  ");
+				
+			}
+			System.out.println(); // line down
+		}
+		
+	}
+
+	public static void printMatt(boolean[][] mat) 
+	{
+		int row_size = mat.length;
+		int col_size = mat[0].length;
+		for( int r = 0 ; r <= row_size ; r++)
+		{
+			for( int c = 0 ; c <= col_size ; c++)
+			{
+				System.out.print(mat[r][c]);
+				System.out.print("  ,  ");
 				
 			}
 			System.out.println(); // line down
